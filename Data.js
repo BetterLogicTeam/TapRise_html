ce80205c9b675c2d8f65cd3cc48edebc8756e6e1
BetterLogicTeam <47892812+tayyabarine@users.noreply.github.com>
2023-07-10 21:29:09 +0500

    Final commit

---
+++ Data.js
@@ -0,0 +1,242 @@
+
+
+
+const Data = [
+  {
+    Image: "./Images/image 1.png",
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+  {
+    Image: "./Images/image 4.png",
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "発売中",
+  },
+  {
+    Image: "./Images/image 6.png",
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+  {
+    Image: "./Images/image 1.png",
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+  {
+    Image: "./Images/image 4.png",
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "発売中",
+  },
+  {
+    Image: "./Images/image 6.png",
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+  {
+    Image: "./Images/image 5.png",
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "発売中",
+  },
+  {
+    Image: "./Images/image 3.png",
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+  {
+    Image: "./Images/image 1.png",
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+  {
+    Image: "./Images/image 6.png",
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "発売中",
+  },
+  {
+    Image: "./Images/image 4.png",
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+
+  {
+    Image: "./Images/image 2.png",
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "発売中",
+  },
+
+  {
+    Image: "./Images/image 1.png",
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "近日発売",
+  },
+  {
+    Image: "./Images/image 3.png",
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "近日発売",
+  },
+  {
+    Image: "./Images/image 5.png",
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "近日発売",
+  },
+  {
+    Image: "./Images/image 2.png",
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "近日発売",
+  },
+  {
+    Image: "./Images/image 6.png",
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "近日発売",
+  },
+  {
+    Image: "./Images/image 4.png",
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "近日発売",
+  },
+  // {
+  //   Image: pic_2,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "近日発売",
+  // },
+  // {
+  //   Image: pic_5,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "近日発売",
+  // },
+  // {
+  //   Image: pic_3,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "近日発売",
+  // },
+  // {
+  //   Image: pic_5,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "近日発売",
+  // },
+  // {
+  //   Image: pic_3,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "近日発売",
+  // },
+  // {
+  //   Image: pic_5,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "近日発売",
+  // },
+  // {
+  //   Image: pic_6,
+  //   Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+  //   tag: "販売終了",
+  // },
+
+  // {
+  //   Image: pic_5,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "販売終了",
+  // },
+  // {
+  //   Image: pic_5,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "販売終了",
+  // },
+
+  // {
+  //   Image: pic_5,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "販売終了",
+  // },
+
+  // {
+  //   Image: pic_3,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "販売終了",
+  // },
+
+
+
+  // {
+  //   Image: pic_1,
+  //   Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+  //   tag: "販売終了",
+  // },
+  // {
+  //   Image: pic_6,
+  //   Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+  //   tag: "販売終了",
+  // },
+  // {
+  //   Image: pic_2,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "販売終了",
+  // },
+  // {
+  //   Image: pic_1,
+  //   Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+  //   tag: "販売終了",
+  // },
+  // {
+  //   Image: pic_2,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "販売終了",
+  // },
+  // {
+  //   Image: pic_1,
+  //   Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+  //   tag: "販売終了",
+  // },
+  // {
+  //   Image: pic_2,
+  //   Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+  //   tag: "販売終了",
+  // },
+];
+
+
+
+
+
+var cards = [
+  {
+    imageUrl: "image1.jpg",
+    title: "Card 1"
+  },
+  {
+    imageUrl: "image2.jpg",
+    title: "Card 2"
+  },
+  {
+    imageUrl: "image3.jpg",
+    title: "Card 3"
+  }
+];
+
+// Function to create card elements
+function createCardElement(card) {
+  var cardDiv = document.createElement("div");
+  cardDiv.className = "card";
+
+  var image = document.createElement("img");
+  image.className = "card-image";
+  image.src = card.imageUrl;
+  image.alt = card.title;
+
+  var title = document.createElement("h3");
+  title.textContent = card.title;
+
+  cardDiv.appendChild(image);
+  cardDiv.appendChild(title);
+  return cardDiv;
+}
+
+// Get the card container element
+var cardContainer = document.getElementById("cardContainer");
+
+// Use map() to create card elements and append them to the container
+var cardElements = cards.map(function(card) {
+  return createCardElement(card);
+});
+
+cardElements.forEach(function(card) {
+  cardContainer.appendChild(card);
+});
